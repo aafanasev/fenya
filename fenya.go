6bcce1a8dacbf2d3a24e8cb186c1bd09092dbdba
jokuskay <tafanasyev@gmail.com>
2018-06-24 21:09:48 +0700

    go lib

---
+++ fenya.go
@@ -0,0 +1,44 @@
+package fenya
+
+import (
+	"log"
+
+	"gopkg.in/telegram-bot-api.v4"
+)
+
+var bot *tgbotapi.BotAPI
+
+func Init(token, baseUrl string, debug bool) error {
+	var err error
+
+	// create a bot
+	bot, err = tgbotapi.NewBotAPI(token)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("[%s] connected", bot.Self.UserName)
+
+	// debug mode
+	bot.Debug = debug
+
+	// set webhook
+	webhookUrl := baseUrl + bot.Token
+	_, err = bot.SetWebhook(tgbotapi.NewWebhook(webhookUrl))
+	if err != nil {
+		return err
+	}
+
+	log.Printf("[%s] set webhook %s", bot.Self.UserName, webhookUrl)
+
+	return err
+}
+
+func Handle(update *tgbotapi.Update) {
+	// TODO: filter NEW MEMBER update
+	const greetingStickerFileId = "CAADAgADQgADaJpdDJ7cv5VPztHoAg"
+
+	msg := tgbotapi.NewStickerShare(update.Message.Chat.ID, greetingStickerFileId)
+
+	bot.Send(msg)
+}
